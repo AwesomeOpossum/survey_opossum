af534459b6509f041eaf9ab9afdb0a545239bd34
Zack Strickland <zas0923@gmail.com>
2015-06-13 11:54:13 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+zack = Author.create!(name: "Zack", email: "ZAS0923@gmail.com", password: "password"  )
