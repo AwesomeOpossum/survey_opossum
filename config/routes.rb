d7067e6b077f6dabe4fe16caa96127bae975dd49
Zack Strickland <zas0923@gmail.com>
2015-06-13 10:55:05 -0400

    Add sessions controller and login/logout routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'surveys#index'
-  
+
+  get 'sessions/login'
+  post 'sessions/login'
+  get 'sessions/logout'
+
   resources :surveys
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
