f8ace14b4524550cc8f13c91c0e24f9da2fc0cc4
Peter McNeary <plmcneary@icloud.com>
2015-06-15 19:13:19 -0400

    Adding dynamically adding questions

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,11 +1,28 @@
 $(function(){
 
-  var newQuestion = $('#templateId').html();
-
  $('.add-button').on('click', function(){
 
-   $('.js-questions-container').append(newQuestion);
+   var lastHtml = $('.question-fields').last().clone();
+   var lastInputs = lastHtml.find('input');
+   var lastIndex = parseInt(lastInputs.first().attr('name').match(/[0-9]+/)[0]);
+
+   var newHtml = lastHtml;
+   var newInputs = newHtml.find('input');
+   var newIndex = (lastIndex+1).toString();
+   newInputs.each(function(i) {
+     var oldName = this.getAttribute('name');
+     this.setAttribute('name', oldName.replace(/[0-9]+/, newIndex));
+     var oldId = this.getAttribute('name');
+     this.setAttribute('id', oldId.replace(/[0-9]+/, newIndex));
+     this.value = ('');
+     //this.attr('name').replace(/[0-9]+/, newIndex);
+   });
+
+   $('.questions-container').append(newHtml);
 
   });
+
+ $('')
+
 
 });
